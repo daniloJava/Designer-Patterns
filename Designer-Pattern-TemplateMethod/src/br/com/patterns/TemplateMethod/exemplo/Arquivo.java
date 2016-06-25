5cc8807b66ab9561942658b3ecc2d1cd708d3e8b
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-06-25 00:20:17 -0300

    Classe te teste do arquivo

---
+++ Designer-Pattern-TemplateMethod/src/br/com/patterns/TemplateMethod/exemplo/Arquivo.java
@@ -0,0 +1,34 @@
+package br.com.patterns.TemplateMethod.exemplo;
+
+/**Uma classe simples de um arquivo qualquer somente para teste.
+ * 
+ * @author Danilo Silva
+ *
+ */
+public class Arquivo {
+
+	private String titulo;
+	private String conteudo;
+
+	
+	
+	public Arquivo(String titulo, String conteudo) {
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	
+	
+}
