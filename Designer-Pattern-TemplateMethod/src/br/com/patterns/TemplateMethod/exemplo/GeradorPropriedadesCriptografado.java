5c11a7c747ae81ed4590045f267762817334c0a8
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-06-24 23:43:44 -0300

    Nova funcionalidade da regra de negocio.

---
+++ Designer-Pattern-TemplateMethod/src/br/com/patterns/TemplateMethod/exemplo/GeradorPropriedadesCriptografado.java
@@ -0,0 +1,47 @@
+package br.com.patterns.TemplateMethod.exemplo;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**Da mesma forma, implementado uma nova funcionalidade
+ * sem nenhum problema.
+ * 
+ * O metodo de Criptografia é usado o conhecido 
+ * cifra de César
+ * 
+ * @author Danilo Silva
+ *
+ */
+public class GeradorPropriedadesCriptografado extends GeradorArquivo {
+	
+	private int delay;
+	
+	
+	public GeradorPropriedadesCriptografado(int delay) {
+		this.delay = delay;
+	}
+	
+	/**Processa de aplicando a criptografia cifra de César
+	 * simplismente em que o valor de cada byte é deslocado de acordo
+	 * com a variavel delay
+	 */
+	protected byte[] processar(byte[] bytes) throws IOException {
+		byte[] newBytes = new byte[bytes.length];
+		for (int i = 0; i < bytes.length; i++) {
+			newBytes[i] = (byte) ((bytes[i] + delay) % Byte.MAX_VALUE);
+		}
+		return newBytes;
+	}
+
+	/**Gera o conteudo Criptografado
+	 * 
+	 */
+	protected String gerarConteudo(Map<String, Object> props) {
+		StringBuilder propFileBuilder = new StringBuilder();
+		for (String prop : props.keySet()) {
+			propFileBuilder.append(prop + "=" + props.get(prop) + "\n");
+		}
+		return propFileBuilder.toString();
+	}
+	
+}
